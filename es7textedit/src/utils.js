324e120278bbf054b520a6ad9a786978a8900057
gabriel <777canaan@gmail.com>
2017-10-17 14:33:39 +1300

    random text renders and new style rules apply

--- es7textedit/src/utils.js
+++ es7textedit/src/utils.js
@@ -1,27 +1,91 @@
+
+class Random {
+  constructor(max = 1000, allowNegitives = true) {
+    this.max = max
+    this.allowNegitives = allowNegitives
+  }
+  randomInt(min, max) {
+    if (max > this.max) {
+      max = this.max
+    }
+    if (min < 0 && !this.allowNegitives) {
+      min = 0
+    }
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max -min +1)) + min
+  }
+}
+
+class colorGenerator extends Random {
+  constructor(max, allowNegitives, type = 'rgb') {
+    super(max, allowNegitives)
+    if (this.typeList.includes(type)) {
+      this.type = type
+    } else {
+      this.type = 'rgb'
+    }
+  }
+  typeList = ['hex', 'rgb']
+
+  get types() {
+    return this.typeList
+  }
+  set types(types = ['hex', 'rgb']) {
+    if (Array.isArray(types)) {
+      this.typeList = types.map(type => type)
+    }
+  }
+
+  color() {
+    let r = super.randomInt(0, 256)
+    let g = super.randomInt(0, 256)
+    let b = super.randomInt(0, 256)
+    if (this.type === 'hex') {
+      return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`
+    } else {
+      return `rgb(${r}, ${g}, ${b})`
+    }
+  }
+}
+
+export const rando =  new colorGenerator()
+
 const shakespeareApi ='https://api.graph.cool/simple/v1/shakespeare'
 
 let options = () => {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    query: `{
-      allPoems(
-        first: 1
-        skip: ${randomInt(0, 160)}
-      ) {
-        title
-        author
-        lines
-        text
-      }
-    }`
-  })
+  return{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `{
+        allPoems(
+          first: 1
+          skip: ${rando.randomInt(0, 160)}
+        ) {
+          title
+          author
+          lines
+          text
+        }
+      }`
+    })
+  }
 }
 
-function randomInt(min, max){
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max -min +1)) + min
+export async function getRandomPoem() {
+  try {
+    let result = await fetch(shakespeareApi, options())
+    let responce = await result.json()
+    let poem = responce.data.allPoems[0]
+    return poem.text
+  } catch (error) {
+    console.log('Error in getRandomPoem, error')
+    throw error
+  }
 }
+// fetch(shakespeareApi, options()).then(responce => responce.json()).then(json => {
+//   console.log(json)
+// })
